4b90234ee7cbfce8e7ce59c017e605510ab400e7
rahul <degrarahul3@gmail.com>
2025-02-05 17:21:30 +0530

    minor change

--- db/migrate/20250204055832_create_products.rb
+++ db/migrate/20250204055832_create_products.rb
@@ -1,6 +1,6 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
-    unless table_exists?(:products) 
+    unless table_exists?(:products)
       create_table :products, primary_key: [ :customer_id, :product_sku ] do |t|
         t.string :name
         t.string :part_number
