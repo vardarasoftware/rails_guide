cc4deeb3a6c524576997b3b01d1403e44766e0e5
rahul <degrarahul3@gmail.com>
2025-02-27 12:24:57 +0530

    fix issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_065348) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_27_065209) do
   create_table "account_histories", force: :cascade do |t|
     t.integer "credit_rating"
     t.integer "account_id", null: false
