cc20e081fa67a7b773569d2a65e50b295521716d
rahul <degrarahul3@gmail.com>
2025-03-05 13:33:15 +0530

    minor changes in note_book_controller

--- app/controllers/note_books_controller.rb
+++ app/controllers/note_books_controller.rb
@@ -1,22 +1,19 @@
 class NoteBooksController < ApplicationController
   require "csv"
-  
+
   def upload_csv
     uploaded_file = params[:csv_file]
-      
     if uploaded_file.present?
       csv_data = CSV.parse(uploaded_file.read, headers: true)
-        
       csv_data.each do |row|
         NoteBook.create(title: row["Title"], description: row["Description"])
       end
-        
       redirect_to notebooks_path, notice: "CSV uploaded successfully!"
     else
       redirect_to notebooks_path, alert: "Please select a file."
     end
   end 
-  
+
   def index
     @note_books = NoteBook.all
   end
@@ -25,8 +22,8 @@ class NoteBooksController < ApplicationController
     @note_book = NoteBook.new
     @authors = Author.all.pluck(:first_name, :last_name, :id)
     @categories = {
-      "Fiction" => [[ "Novel", "novel" ], [ "Poetry", "poetry" ]],
-      "Non-Fiction" => [[ "Biography", "biography" ], [ "Science", "science" ]]
+      "Fiction" => [ [ "Novel", "novel" ], [ "Poetry", "poetry" ] ],
+      "Non-Fiction" => [ [ "Biography", "biography" ], [ "Science", "science" ] ]
     }
   end
 
